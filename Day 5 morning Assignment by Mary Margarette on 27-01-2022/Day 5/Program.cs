ca5752688da80bf589c8482b3db58637a40113e7
Mary Margarette <mamuduri.margarette222@gmail.com>
2022-01-28 19:02:56 +0530

    NH Assignments

---
+++ Day 5 morning Assignment by Mary Margarette on 27-01-2022/Day 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAay_5
+{
+    internal class Program
+    {
+        class Products
+        {
+            private int productid;
+            private int productname;
+            private int quantity;
+            private int price;
+
+            public void Add Product Details()
+            {
+                // TO DO
+            }
+        }
+        
+    }
+}

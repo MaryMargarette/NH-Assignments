f6dab0439216226ed8fc3438d11293be1c157a46
Mary Margarette <mamuduri.margarette222@gmail.com>
2022-01-31 18:52:03 +0530

    NH Assignments

---
+++ Day 6 morning Assignment by Mary Margarette on 31-01-2022/Day 6 Project 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day_6_Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList data = new ArrayList();
+            int sum = 0;
+            data.Add(50);
+            data.Add(61);
+            data.Add(72);
+            data.Add(83);
+            data.Add(94);
+
+            foreach (var d in data)
+            { 
+                sum =sum + (int) d;
+            }
+        }
+    }
+}
---
+++ Day 6 morning Assignment by Mary Margarette on 31-01-2022/Day 6 project 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = new List<int>();
+
+            int temp;
+            int sum1 = 0, sum2 = 0;
+
+            // print values using foreach loop
+             for (int i = 0;i<data.Count;i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+
+            //print values using foreach loop
+
+             foreach (var d in data)
+            {
+                Console.WriteLine(d);
+            }
+
+            // print values using lambda function
+            data.ForEach(p => Console.WriteLine(p));
+
+            Console.ReadLine();
+        }
+    }
+}

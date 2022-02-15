ffe95cf7ab94d3004669e42d5d88bf87954123b7
Mary Margarette <mamuduri.margarette222@gmail.com>
2022-02-16 00:01:52 +0530

    NH Assignments

---
+++ Day 17 Assignment by Mary Margarette on 15-02-2022/MarLibrary/Derived.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MargaretLibrary;
+
+namespace MarLibrary
+{
+    public class Derived : MyBaseClass
+    {
+        public void Write()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+    }
+    public class Otherclass
+    {
+        public void Write()
+        {
+            MyBaseClass m = new MyBaseClass();
+            m.a = 1;
+            m.b = 2;
+            m.c = 3;
+            m.d = 4;
+            m.e = 5;
+        }
+        
+
+    }
+}
---
+++ Day 17 Assignment by Mary Margarette on 15-02-2022/MargaretLibrary/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MargaretLibrary
+{
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+
+
+        public void ReadValue()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+            d = 40;
+            e = 50;
+
+        }
+    }
+
+    
+    class Derivedclass : MyBaseClass
+    {
+        public void ReadValue()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+            d = 40;
+            e = 50;
+
+        }
+    }
+
+   class Otherclass
+    {
+        public void ReadValue()
+        {
+            MyBaseClass m = new MyBaseClass();
+            m.a = 10;
+            m.b = 20;
+            m.c = 30;
+            m.d = 40;
+            m.e = 50;
+
+
+        }
+    }
+}

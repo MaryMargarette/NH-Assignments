d4fd78d9f991e06e491ba9e4e725d9d790aa769a
Mary Margarette <mamuduri.margarette222@gmail.com>
2022-02-21 11:43:12 +0530

    NH Assignmnets

---
+++ Day 21 Assignment by Mary Margarette on 21-02-2022/WebApplication1/WebApplication1/Web1.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for Web1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Web1 : System.Web.Services.WebService
+    {
+
+        
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return a+b;
+        }
+
+        [WebMethod]
+        public int Mul(int c, int d)
+        {
+            return c*d;
+        }
+
+        [WebMethod]
+        public int Div(int e, int f)
+        {
+            return e / f;
+        }
+
+        [WebMethod]
+        public int Factorial(int n)
+        {
+            int fact = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                fact = fact*i;
+            }
+            return fact;
+        }
+    }
+}
---
+++ Day 21 Assignment by Mary Margarette on 21-02-2022/WebC0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebC0.ServiceReference1;
+
+namespace WebC0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Web1SoapClient wb = new Web1SoapClient();
+            Console.WriteLine(wb.Factorial(5));
+            Console.WriteLine(wb.Add(5,6));
+            Console.WriteLine(wb.Mul(5,5));
+            Console.WriteLine(wb.Div(8,2));
+
+            Console.ReadLine();
+
+        }
+    }
+}

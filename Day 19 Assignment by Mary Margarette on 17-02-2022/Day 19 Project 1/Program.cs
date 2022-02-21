42441df0a85d2460e5ae2ffe0041cd237ca50ef9
Mary Margarette <mamuduri.margarette222@gmail.com>
2022-02-21 06:45:59 +0530

    NH Assignments

--- Day 19 Assignment by Mary Margarette on 17-02-2022/Day 19 Project 1/Program.cs
+++ Day 19 Assignment by Mary Margarette on 17-02-2022/Day 19 Project 1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Day_19_Project_1
 {
@@ -10,6 +11,10 @@ namespace Day_19_Project_1
     {
         static void Main(string[] args)
         {
+            XmlDocument xml = new XmlDocument();
+            xml.Load("E:\\NH Assignments\\Day 18 Assignment by Mary Margarette on 16-02-2022\\Tag.xml");
+            xml node = xml.DocumentElement.SelectSingleNode("//employees//employee");
+            foreach(XmlNode node2 in .Documentelement.ChildNodes)
 
         }
     }
---
+++ Day 20 Assignment by Mary Margarette on 18-02-2022/Day 20 Project 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20_Project_1
+{
+    internal class Program
+    {
+        public static void Add(out int a, out int b)
+        {
+            a =15;
+            b = 26;
+        }
+        static void Main(string[] args)
+        {
+            int i, j;
+            Add(out i, out j);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.ReadLine();
+
+
+        }
+    }
+}
